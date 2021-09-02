c287962c65fb33989b9fbfcaba98a447f8df7117
Yuriy Belov <paganism@mail.ru>
2021-09-02 10:04:08 +0300

    Show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -26,10 +26,14 @@ def serialize_tag(tag):
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
     }
 
+def get_likes_count(post):
+    return Post.objects.get(pk=post.pk).likes.count()
 
 def index(request):
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+    posts = Post.objects.all()
+    popular_posts = sorted(posts, key=get_likes_count)
+    most_popular_posts = popular_posts[-5:]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
