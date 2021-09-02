b85b4182f01f966c9953d461952fd3dfc766283a
Yuriy Belov <paganism@mail.ru>
2021-09-02 17:23:00 +0300

    Replace sorted func with order_by

--- blog/views.py
+++ blog/views.py
@@ -32,9 +32,8 @@ def get_likes_count(post):
 
 def index(request):
 
-    posts = Post.objects.annotate(Count('likes'))
-    popular_posts = sorted(posts, key=get_likes_count)
-    most_popular_posts = popular_posts[-5:]
+    posts = Post.objects.annotate(likes_count=Count('likes')).order_by('-likes_count')
+    most_popular_posts = posts[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
